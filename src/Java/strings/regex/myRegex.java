7fc63767282ef4eb24378caebab6982bfbf28958
kunal-das <kunaldasece@gmail.com>
2015-05-28 08:30:41 +0530

    Solution for java->strings->regex problem

---
+++ src/Java/strings/regex/myRegex.java
@@ -0,0 +1,11 @@
+package Java.strings.regex;
+
+class myRegex {
+	
+	String pattern;
+	
+	myRegex()
+	{
+		pattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+	}
+}
